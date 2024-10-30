3276efef5e2040cf2cbf5db2ecc2c324d02cae7b
usein <useinakbar01@gmail.com>
2024-10-30 15:01:25 +0700

    b-test: commit branch test

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// this is branch test
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
